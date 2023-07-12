81c611bbe853a4f6f2ef018609e411db6cefaeaa
appworld-hu <burai.sandor@gmail.com>
2023-07-12 17:46:38 +0200

    Added status arg with default value to handleCase

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
     
-    const handleCase = (res, file) => {
-        res.writeHead(200, {'Content-Type': 'text/html'})
+    const handleCase = (res, file, status=200) => {
+        res.writeHead(status, {'Content-Type': 'text/html'})
         fs.createReadStream(__dirname+"/"+file+".html").pipe(res)
     }
 
@@ -16,8 +16,7 @@ const server = http.createServer((req, res)=>{
             handleCase(res, "about")
         break;
         default:
-            res.writeHead(404, {'Content-Type': 'text/html'})
-            fs.createReadStream(__dirname+"/404.html").pipe(res);
+            handleCase(res, "404", 404)
         break;
     }
 })
